c6720636e577c4ab2632b6d0bcae06978b615933
Lin <linmaweb@gmail.com>
2020-08-28 20:15:35 +1000

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,21 +2,25 @@ import { createNote } from "./notes";
 import { setFilters } from "./filters";
 import { renderNotes } from "./views";
 
+const createNoteElement = document.querySelector("#create-note");
+const searchTextElement = document.querySelector("#search-text");
+const filterByElement = document.querySelector("#filter-by");
+
 renderNotes();
 
-document.querySelector("#create-note").addEventListener("click", (e) => {
+createNoteElement.addEventListener("click", (e) => {
   const id = createNote();
   location.assign(`/edit.html#${id}`);
 });
 
-document.querySelector("#search-text").addEventListener("input", (e) => {
+searchTextElement.addEventListener("input", (e) => {
   setFilters({
     searchText: e.target.value,
   });
   renderNotes();
 });
 
-document.querySelector("#filter-by").addEventListener("change", (e) => {
+filterByElement.addEventListener("change", (e) => {
   setFilters({
     sortBy: e.target.value,
   });
